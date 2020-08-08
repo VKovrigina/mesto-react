5515b5e1d30a7ab64b91a308fc2dee4d19c90bc9
Varvara Kovrigina <Onlimusik@yandex.ru>
2020-08-08 20:24:07 +0300

    feat: add comp PopupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,36 +1,14 @@
 import React from 'react';
 
-function PopupWithForm() {
+function PopupWithForm({ name ,title ,buttonText }) {
   return (
-    <div className="popup popup_profile popup_form">
+    <div className={`popup popup_${name} popup_form`}>
     <div className="popup__container">
       <button className="popup__close-button button" type="button" aria-label="Закрыть"></button>
-      <form className="popup__form popup__form_profile" name="form-profile" method="get" action="#" novalidate>
-        <h2 className="popup__title">Редактировать профиль</h2>
+      <form className={`popup__form popup__form_${name}`} name={name} method="get" action="#" novalidate>
+        <h2 className="popup__title">{title}</h2>
 
-        <input
-          type="text"
-          name="name"
-          className="popup__input popup__input_type_name"
-          id="name-input"
-          placeholder="ФИО"
-          minlength="2"
-          maxlength="40"
-        required />
-        <span className="popup__input-error" id="name-input-error"></span>
-
-        <input
-          type="text"
-          name="about"
-          className="popup__input popup__input_type_job"
-          id="job-input"
-          placeholder="О себе"
-          minlength="2"
-          maxlength="200"
-        required />
-        <span className="popup__input-error" id="job-input-error"></span>
-
-        <button className="popup__form-button" type="submit">Сохранить</button>
+        <button className="popup__form-button" type="submit" aria-label={buttonText}>{buttonText}</button>
       </form>
     </div>
   </div>
