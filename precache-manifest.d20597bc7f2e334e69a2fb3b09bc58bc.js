bdb45116186157af3bbe19dcc9afac58cbba7827
Varvara Kovrigina <Onlimusik@yandex.ru>
2020-08-12 13:22:43 +0300

    Updates

---
+++ precache-manifest.d20597bc7f2e334e69a2fb3b09bc58bc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d05177dea764766901254b2803046e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d921a88783f12fadbe9",
+    "url": "./static/css/main.75e0fa8c.chunk.css"
+  },
+  {
+    "revision": "b585261e53e3992d89d0",
+    "url": "./static/js/2.6be62bd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6be62bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d921a88783f12fadbe9",
+    "url": "./static/js/main.b27afdf9.chunk.js"
+  },
+  {
+    "revision": "a8ef13021948c1ee7e9c",
+    "url": "./static/js/runtime-main.a5163ae5.js"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "./static/media/add-no-bord.b2fce4ac.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "./static/media/big-edit.d476a8b8.svg"
+  },
+  {
+    "revision": "d80783439d382b46ce24b8da369767d4",
+    "url": "./static/media/card-delete.d8078343.svg"
+  },
+  {
+    "revision": "be38ad19bc0006f9a08ce139c75dda92",
+    "url": "./static/media/close-icon-little.be38ad19.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "./static/media/close-icon.5df32a06.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "./static/media/edit.6bfdb354.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "./static/media/like-active.5bbb05f7.svg"
+  },
+  {
+    "revision": "2b1f7ee54f51648eafe2b619a0378ea4",
+    "url": "./static/media/logo.2b1f7ee5.svg"
+  },
+  {
+    "revision": "83a46f45b0ce22c9a4433c11d0372f26",
+    "url": "./static/media/vector.83a46f45.svg"
+  }
+]);
