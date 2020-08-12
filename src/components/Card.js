084b5a4e15fb6cbd9aa18e0f7ebf62daf97dfe7e
Varvara Kovrigina <Onlimusik@yandex.ru>
2020-08-12 11:57:19 +0300

    style: tab

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,22 @@
 import React from "react";
 
 function Card({ name, link, likes, onCardClick }) {
-  
+
   function handleClick() {
     onCardClick(name, link);
   }
 
   return (
     <article className="card">
-            <button className="card__button-delete" type="button" aria-label="Удалить"></button>
-            <img className="card__img" alt={name} src={link} onClick={handleClick} />
-            <div className="card__info">
-            <h2 className="card__title">{name}</h2>
-            <div className="card__likes">
-                <button className="card__button-like" type="button" aria-label="Нравится"></button>
-                <div className="card__num-likes">{likes}</div>
-            </div>
-            </div>
+      <button className="card__button-delete" type="button" aria-label="Удалить"></button>
+      <img className="card__img" alt={name} src={link} onClick={handleClick} />
+      <div className="card__info">
+        <h2 className="card__title">{name}</h2>
+          <div className="card__likes">
+            <button className="card__button-like" type="button" aria-label="Нравится"></button>
+            <div className="card__num-likes">{likes}</div>
+          </div>
+      </div>
     </article>
   );
 }
--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -14,18 +14,18 @@ function ImagePopup({card, onClose}) {
 
   return (
     <div className="popup popup_photo">
-        <div className="popup__container">
-          <button 
-            className="popup__close-button button"
-            type="button" 
-            aria-label="Закрыть"
-            onClick={() => {onClose(); removeOpenClass()}}>
-          </button>
-          <img
-            className="popup__img"
-            alt={card.name} src={card.link} />
-          <p className="popup__photo-title">{card.name}</p>
-        </div>
+      <div className="popup__container">
+        <button 
+          className="popup__close-button button"
+          type="button" 
+          aria-label="Закрыть"
+          onClick={() => {onClose(); removeOpenClass()}}>
+        </button>
+        <img
+          className="popup__img"
+          alt={card.name} src={card.link} />
+        <p className="popup__photo-title">{card.name}</p>
+      </div>
     </div>
   );
 }
